79053998b48f61adbe1672a3018eef0a0b7c29b7
yaojintao <yaojintao@zuoyebang.com>
2020-09-22 20:04:14 +0800

    添加了request请求

---
+++ src/utils/request.ts
@@ -0,0 +1,27 @@
+class Request {
+  url; // 请求url
+  parame; // 请求参数
+  constructor (url: string, parame: any) {
+    this.url = url
+    this.parame = parame
+  }
+
+  get () {
+    this.req('GET')
+  }
+
+  post () {
+    this.req('POST')
+  }
+
+  req (method: any) {
+    uni.request({
+      url: this.url,
+      method: method,
+      data: this.parame,
+      timeout: 30000
+    })
+  }
+}
+
+export default Request
