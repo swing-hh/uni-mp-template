072576bc89039e6793f06f863f5f8ddee6642c93
yaojintao <yaojintao@zuoyebang.com>
2020-10-02 21:56:59 +0800

    增加了log.ts

---
+++ src/utils/log.ts
@@ -0,0 +1,20 @@
+export default function log(obj = {}) {
+  const defaultObj = {
+    wxUser: uni.getStorageSync('wx_user'),
+    wxUid: uni.getStorageSync('wx_uid'),
+    lastfrom: getApp().globalData,
+    cuid: uni.getStorageSync('openid')
+  }
+  let str: string = ''
+
+  Object.assign(defaultObj, obj)
+  Object.keys(defaultObj).forEach(item => {
+    str += `${item}=${defaultObj[item]}&`
+  })
+  const arr: Array<string> = str.split('')
+  arr.pop()
+  str = arr.join('')
+  uni.request({
+    url: 'https://yayaxiezi.zybang.com/xiezilog/yayaxiezi_log?' + str
+  })
+}
